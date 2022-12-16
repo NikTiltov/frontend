a4e93b5c16867c026f3e61f674c3b16574cdfde5
NikolaPedro <atron4ik@gmail.com>
2022-12-16 19:41:45 +1000

    add Pallete and Colors components

---
+++ src/lib/palette/ColorPanel.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+   import { onMount } from "svelte";
+   import updateColor from "./updateColor";
+
+   export let colorName = "";
+   export let value = "#ffffff";
+
+   onMount(() => updateColor(colorName, value));
+</script>
+
+<label>
+   <input type="color" bind:value on:change={() => updateColor(colorName, value)} />
+</label>
+
+<style>
+   label {
+      width: 52px;
+      height: 52px;
+      cursor: pointer;
+      display: inline-block;
+      border: 2px solid #000000;
+      border-radius: 50px;
+      overflow: hidden;
+   }
+   
+   input {
+      width: 100%;
+      height: 100%;
+      cursor: pointer;
+      overflow: hidden;
+      border: 3px solid #ffffff;
+      border-radius: 50px;
+   }
+
+   input[type=color]::-moz-focus-inner {
+      border: none;
+   }
+
+   input[type=color]::-moz-color-swatch {
+      border: none;
+   }
+
+</style>
---
+++ src/lib/palette/Palette.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+   import ColorPanel from "./ColorPanel.svelte";
+
+   let colors = [
+      {colorName: "base", value: "#ffffff"},
+      {colorName: "text", value: "#000000"},
+      {colorName: "contrast", value: "#2e2c29"},
+      {colorName: "primary", value: "#4b62dc"},
+      {colorName: "secondary", value: "#ab62e5"},
+   ]
+</script>
+
+<div class="palette">
+   {#each colors as {...params}}
+      <ColorPanel {...params} />
+   {/each}
+</div>
+
+<style>
+   .palette {
+      display: flex;
+      flex-direction: column;
+      gap: 12px;
+   }
+</style>
