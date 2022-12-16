dae2a9a351cc57490e0b83f6650d0fcab984a13f
NikolaPedro <atron4ik@gmail.com>
2022-12-16 19:44:12 +1000

    add editor page

---
+++ src/lib/View.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+   import type { block, text, image } from "./blocks/types";
+   import { default_struct, default_texts, default_images } from "./preset";
+   import Block from "$lib/blocks/Block.svelte";
+   import Element from "./blocks/Element.svelte";
+   import TextEditElement from "./blocks/TextEditElement.svelte";
+   import ImageElement from "./blocks/ImageElement.svelte";
+
+   let blocks: block[] = default_struct;
+   let texts: text[] = default_texts;
+   let images: image[] = default_images;
+</script>
+
+<div class="view">
+   {#each blocks as block}
+      <Block>
+         {#each block as element}
+         <Element>
+            {#if element.type === "text"}
+               <TextEditElement data={texts[element.id]}></TextEditElement>
+            {:else if element.type === "image"}
+               <ImageElement data={images[element.id]}></ImageElement>
+            {/if}
+         </Element>
+         {/each}
+      </Block>
+   {/each}
+</div>
+
+<style>
+   .view {
+      max-width: 1000px;
+      display: flex;
+      flex-direction: column;
+      margin: 0 auto;
+      padding: 60px 10px;
+      gap: 30px;
+   }
+</style>
+
---
+++ src/routes/creator/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+   import Palette from "$lib/palette/Palette.svelte";
+   import View from "$lib/View.svelte";
+</script>
+
+<div class="container">
+   <View />
+   <div class="tool right"><Palette /></div>
+</div>
+
+<style>
+   .container {
+      min-height: 100vh;
+      background-color: var(--custom-color-base);
+   }
+
+   .tool {
+      position: fixed;
+      top: 50%;
+      transform: translateY(-50%);
+   }
+
+   .left {
+      left: 20px;
+   }
+
+   .right {
+      right: 20px;
+   }
+</style>
