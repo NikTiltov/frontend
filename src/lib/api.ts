980cb3aab88c9b8a7e9f19671015444eb4f743c8
NikolaPedro <atron4ik@gmail.com>
2022-12-16 21:02:17 +1000

    hotfix

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,9 @@
-const API_HOST = "";
+const API_HOST = "http://localhost:8000";
 
 export function getPortfolio(user_name: string, portfolio_name: string) {
-   return fetch(`${API_HOST}/${user_name}/${portfolio_name}`)
+   // return fetch(`${API_HOST}/${user_name}/${portfolio_name}`)
+   //    .then(responce => responce.json());
+   return fetch(`${API_HOST}/portfolio/open`)
       .then(responce => responce.json());
 }
 
