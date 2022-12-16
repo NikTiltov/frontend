a8d0941fb6b5f97693b1e99b811f4610745fb450
NikolaPedro <atron4ik@gmail.com>
2022-12-16 19:43:48 +1000

    add page for portfolio view

---
+++ src/routes/[username]/[portfolio]/+page.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+   import type { PageData } from "./$types";
+   import { onMount } from "svelte";
+   import Block from "$lib/blocks/Block.svelte";
+   import Element from "$lib/blocks/Element.svelte";
+   import TextElement from "$lib/blocks/TextElement.svelte";
+   import ImageElement from "$lib/blocks/ImageElement.svelte";
+   import updateColor from "$lib/palette/updateColor";
+
+   export let data: PageData;
+
+   onMount(() => {
+      Object.entries(data.colors)
+         .forEach(([color, value]) => updateColor(color, value));
+   });
+</script>
+
+<div class="view">
+   {#each data.struct as block}
+      <Block>
+         {#each block as element}
+         <Element>
+            {#if element.type === "text"}
+               <TextElement data={data.texts[element.id]}></TextElement>
+            {:else if element.type === "image"}
+               <ImageElement data={data.images[element.id]}></ImageElement>
+            {/if}
+         </Element>
+         {/each}
+      </Block>
+   {/each}
+</div>
+
+<style>
+   .view {
+      max-width: 1000px;
+      display: flex;
+      flex-direction: column;
+      margin: 0 auto;
+      padding: 60px 10px;
+      gap: 30px;
+   }
+</style>
---
+++ src/routes/[username]/[portfolio]/+page.ts
@@ -0,0 +1,18 @@
+import type { PageLoad } from "./$types";
+import { getPortfolio } from "$lib/api";
+import { 
+   default_struct, 
+   default_texts, 
+   default_images,
+   default_colors
+} from "$lib/preset";
+
+export const load: PageLoad = (({ params }) => {
+   return {
+      struct: default_struct,
+      colors: default_colors,
+      texts: default_texts,
+      images: default_images,
+   };
+   // return getPortfolio(params.username, params.portfolio);
+});
