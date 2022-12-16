dc2ea56eae9f5bf0db80c0cfebf3727d758fdd14
NikolaPedro <atron4ik@gmail.com>
2022-12-16 19:39:27 +1000

    add button component

---
+++ src/lib/Button.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+   export let action = () => {};
+</script>
+
+<button on:click|preventDefault={action}>
+   <slot></slot>
+</button>
+
+<style>
+   button {
+      cursor: pointer;
+      color: var(--color-fg);
+      background-color: var(--color-bg);
+      border: 2px solid var(--color-fg);
+      padding: 100px 120px;
+      transform: rotate3d();
+   }
+</style>
