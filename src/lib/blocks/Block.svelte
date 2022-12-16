33b42980db4f54ff2575bcdc6f84237e727f2887
NikolaPedro <atron4ik@gmail.com>
2022-12-16 19:39:45 +1000

    add block element

---
+++ src/lib/blocks/Block.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+   import Element from "./Element.svelte";
+</script>
+
+<div class="block">
+   <slot></slot>
+</div>
+
+<style>
+   .block {
+      width: 100%;
+      padding: 10px;
+      display: flex;
+      flex-direction: row;
+      justify-content: stretch;
+      gap: 20px;
+   }
+
+   @media (max-width: 500px) {
+      .block {
+         flex-direction: column;
+         gap: 5px;
+      }
+   }
+</style>
