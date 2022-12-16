306e69ccd4e1c2543c5d0cb5d6464abd22e72ad1
NikolaPedro <atron4ik@gmail.com>
2022-12-16 19:42:41 +1000

    add main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,33 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+   import { goto } from "$app/navigation";
+   import Button from "$lib/Button.svelte";
+</script>
+
+<div class="container">
+   <div class="divider" />
+   <div class="name">Portfolio You</div>
+   <div class="divider" />
+   <div class="divider" />
+   <Button action={() => goto("/creator")}>create</Button>
+</div>
+
+<style>
+   .container {
+      min-height: 100vh;
+      background-color: var(--color-bg);
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+   }
+
+   .name {
+      text-align: center;
+      color: var(--color-fg);
+      font-size: 128px;
+      font-family: 'Roboto mono', monospace;
+   }
+
+   .divider {
+      height: 100px;
+   }     
+</style>
