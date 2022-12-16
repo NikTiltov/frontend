cc2eaa8b5efc530f517599e5de357c0ce2dc9700
NikolaPedro <atron4ik@gmail.com>
2022-12-16 19:40:57 +1000

    add ImageElenment component

---
+++ src/lib/blocks/ImageElement.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+   import type { image } from "./types";
+   import { getImage } from "$lib/api";
+
+   export let data: image;
+</script>
+
+<div class="image">
+   <img src={getImage(data.src)} alt="">
+</div>
+
+<style>
+   .image {
+      width: 100%;
+      padding-top: 100%;
+      position: relative;
+   }
+
+   img {
+      position: absolute;
+      top: 0px;
+      left: 0px;
+      width: 100%;
+      height: 100%;
+      border: 2px solid var(--custom-color-contrast);
+      object-fit: cover;
+   }
+</style>
