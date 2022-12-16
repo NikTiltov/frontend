a18d4e278fbe94d4459204fbc2f2a0966457eb7a
NikolaPedro <atron4ik@gmail.com>
2022-12-16 20:58:30 +1000

    update portfolio responce

--- src/routes/[username]/[portfolio]/+page.ts
+++ src/routes/[username]/[portfolio]/+page.ts
@@ -1,18 +1,8 @@
 import type { PageLoad } from "./$types";
+import type { portfolio } from "$lib/blocks/types";
 import { getPortfolio } from "$lib/api";
-import { 
-   default_struct, 
-   default_texts, 
-   default_images,
-   default_colors
-} from "$lib/preset";
 
-export const load: PageLoad = (({ params }) => {
-   return {
-      structure: default_struct,
-      colors: default_colors,
-      texts: default_texts,
-      images: default_images,
-   };
-   // return getPortfolio(params.username, params.portfolio);
-});
+export const load: PageLoad = async ({ params }) => {
+   const portfolio: portfolio = await getPortfolio(params.username, params.portfolio);
+   return portfolio;
+};
