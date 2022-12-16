a5a2a58e500a6f7771a82b50d348a12e9896215b
NikolaPedro <atron4ik@gmail.com>
2022-12-16 21:43:37 +1000

    add default portfolio

--- src/routes/[username]/[portfolio]/+page.ts
+++ src/routes/[username]/[portfolio]/+page.ts
@@ -1,8 +1,20 @@
 import type { PageLoad } from "./$types";
 import type { portfolio } from "$lib/blocks/types";
 import { getPortfolio } from "$lib/api";
+// import { 
+//    default_struct,
+//    default_colors,
+//    default_texts,
+//    default_images
+// } from "$lib/preset";
 
 export const load: PageLoad = async ({ params }) => {
-   const portfolio: portfolio = await getPortfolio(params.username, params.portfolio);
-   return portfolio;
+   const json: { portfolio: portfolio } = await getPortfolio(params.username, params.portfolio);
+   return json.portfolio;
+   // return {
+   //    structure: default_struct,
+   //    colors: default_colors,
+   //    texts: default_texts,
+   //    images: default_images,
+   // };
 };
