5d7858762a5836c20611ad4320a6ed63b3fb084e
NikolaPedro <atron4ik@gmail.com>
2022-12-18 22:16:49 +1000

    in process

---
+++ src/routes/reg/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+   let files: FileList;
+   let data: any;
+
+   function Image() {
+      let reader = new FileReader();
+      reader.readAsDataURL(files[0]);
+
+      reader.onload = () => {
+         data = reader.result;
+      };
+   }
+</script>
+
+
+<input type="file" bind:files={files}/>
+<button on:click={Image}>generate</button>
+{data}
