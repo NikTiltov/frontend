d3dba3942b8ce50c38f052e0ec0bd1719df929f1
NikolaPedro <atron4ik@gmail.com>
2022-12-18 22:10:38 +1000

    bind color value

--- src/lib/palette/Palette.svelte
+++ src/lib/palette/Palette.svelte
@@ -1,18 +1,17 @@
 <script lang="ts">
+   import type { colors } from "$lib/types";
    import ColorPanel from "./ColorPanel.svelte";
 
-   let colors = [
-      {colorName: "base", value: "#ffffff"},
-      {colorName: "text", value: "#000000"},
-      {colorName: "contrast", value: "#2e2c29"},
-      {colorName: "primary", value: "#4b62dc"},
-      {colorName: "secondary", value: "#ab62e5"},
-   ]
+   export let colors: colors;
+
+   function toColorName(colorName: string) {
+      return (colorName as keyof colors);
+   }
 </script>
 
 <div class="palette">
-   {#each colors as {...params}}
-      <ColorPanel {...params} />
+   {#each Object.keys(colors) as colorName}
+      <ColorPanel {colorName} bind:value={colors[toColorName(colorName)]} />
    {/each}
 </div>
 
