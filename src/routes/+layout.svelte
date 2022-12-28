e86dfb5241edce30543f13659e7cf7bc65bc0a23
NikolaPedro <atron4ik@gmail.com>
2022-12-28 19:30:41 +1000

    add style for main

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,3 +5,14 @@
 <main>
    <slot></slot>
 </main>
+
+<style>
+   main {
+      min-height: 100vh;
+      background-color: var(--color-base);
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+   }
+</style>
