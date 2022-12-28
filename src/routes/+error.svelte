d0bd97810f395a1ecf9a748565957e6e44deff67
NikolaPedro <atron4ik@gmail.com>
2022-12-28 19:30:17 +1000

    add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,31 @@
+<div class="bg">
+   <div class="container">
+      <p class="error-message">The page you are looking for can't be found.</p>
+      <div class="error-code">404</div>
+   </div>
+</div>
+
+<style>
+   .bg {
+      min-height: 100vh;
+      background-color: var(--color-base);
+      display: flex;
+      justify-content: center;
+      align-items: center;
+   }
+
+   .container {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+   }
+
+   .error-message {
+      font-size: var(--font-size-big);
+   }
+
+   .error-code {
+      font-size: 200px;
+      color: var(--color-error);
+   }
+</style>
