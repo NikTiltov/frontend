53d60fe23b9b827522234e18c9115e848d75f18c
NikolaPedro <atron4ik@gmail.com>
2022-12-28 21:14:08 +1000

    fix

--- src/routes/[username]/[portfolio]/+page.ts
+++ src/routes/[username]/[portfolio]/+page.ts
@@ -2,14 +2,14 @@ import type { PageLoad } from "./$types";
 import type { portfolio } from "$lib/types";
 import { error } from "@sveltejs/kit";
 import { env } from "$env/dynamic/public";
-import { default_portfolio } from "$lib/preset";
 
 export const load: PageLoad = async ({ params, fetch }) => {
-   // await fetch(`${env.API_HOST}/${params.username}/${params.portfolio}`, { method: "POST" })
-   return await fetch(`${env.PUBLIC_API_HOST}/portfolio/open`, { method: "POST" })
+   // const path = `${env.PUBLIC_API_HOST}/${params.username}/${params.portfolio}`;
+   const path = `${env.PUBLIC_API_HOST}/portfolio/open`;
+   return await fetch(path, { method: "POST" })
       .then(response => response.json())
       .then(json => json["portfolio"] as portfolio)
-      .catch(err => {
+      .catch(() => {
          throw error(404, { message: "page not found" });
       });
 };
