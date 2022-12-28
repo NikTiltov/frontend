de810a38f028a73f15a3c76a196a32580f2322e3
NikolaPedro <atron4ik@gmail.com>
2022-12-28 19:21:24 +1000

    full rework

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -1,18 +1,52 @@
 <script lang="ts">
-   export let action = () => {};
+   export let color: Color = "primary";
+   export let size: Size = "medium";
+   export let shadow: Shadow = "none";
+
+   type Color =
+      | "primary"
+      | "success"
+      | "warning"
+      | "error";
+
+   type Size =
+      | "medium"
+      | "big";
+
+   type Shadow =
+      | "left"
+      | "right"
+      | "none";
 </script>
 
-<button on:click|preventDefault={action}>
+<button on:click
+   class="{size} shadow-{shadow}" 
+   style="background: var(--color-{color})">
    <slot></slot>
 </button>
 
 <style>
    button {
-      cursor: pointer;
-      color: var(--color-fg);
-      background-color: var(--color-bg);
-      border: 2px solid var(--color-fg);
-      padding: 100px 120px;
-      transform: rotate3d();
+      box-shadow: 0px 0px var(--color-dark);
+      border: 2px solid var(--color-dark);
+      border-radius: 16px;
+   }
+   
+   .big {
+      padding: 10px 20px;
+      font-size: var(--font-size-big);
+   }
+   
+   .medium {
+      padding: 8px 16px;
+      font-size: var(--font-size-medium);
+   }
+   
+   .shadow-left {
+      box-shadow: -10px 0px var(--color-dark);
+   }
+   
+   .shadow-right {
+      box-shadow: 10px 0px var(--color-dark);
    }
 </style>
